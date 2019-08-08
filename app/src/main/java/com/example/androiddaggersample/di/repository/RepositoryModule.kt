afb2d0374b35bf0e25e187bc7289fea2b3c9d517
Rapbong <rapbong@gmail.com>
2019-08-08 22:56:31 +0900

    Remove unused method

--- app/src/main/java/com/example/androiddaggersample/di/repository/RepositoryModule.kt
+++ app/src/main/java/com/example/androiddaggersample/di/repository/RepositoryModule.kt
@@ -1,6 +1,9 @@
 package com.example.androiddaggersample.di.repository
 
+import androidx.paging.DataSource
+import com.example.androiddaggersample.repository.RepoDataSource
 import com.example.androiddaggersample.repository.data.GithubService
+import com.example.androiddaggersample.repository.data.Repo
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
@@ -14,14 +17,22 @@ import javax.inject.Singleton
 class RepositoryModule {
     @Singleton
     @Provides
-    fun provideGithubService(): GithubService {
+    fun provideGithubService(gsonConverterFactory: GsonConverterFactory,
+                             rxJava2CallAdapterFactory: RxJava2CallAdapterFactory)
+            : GithubService {
         return Retrofit.
             Builder().baseUrl("https://api.github.com/")
             .client(provideHttpClient())
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(gsonConverterFactory)
+            .addCallAdapterFactory(rxJava2CallAdapterFactory)
             .build()
             .create(GithubService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideDataSource(githubService: GithubService): DataSource<Int, Repo> {
+        return RepoDataSource(githubService)
     }
 
     @Singleton
@@ -30,5 +41,17 @@ class RepositoryModule {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor())
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRxjavaCallAdapterFactory(): RxJava2CallAdapterFactory {
+        return RxJava2CallAdapterFactory.create()
     }
 }
--- app/src/main/java/com/example/androiddaggersample/repository/GithubRepository.kt
+++ app/src/main/java/com/example/androiddaggersample/repository/GithubRepository.kt
@@ -7,9 +7,6 @@ import androidx.paging.PagedList
 import com.example.androiddaggersample.TAG
 import com.example.androiddaggersample.repository.data.GithubService
 import com.example.androiddaggersample.repository.data.Repo
-import com.example.androiddaggersample.repository.data.RepoSearchResponse
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class GithubRepository @Inject constructor(private val githubService: GithubService){
@@ -23,9 +20,5 @@ class GithubRepository @Inject constructor(private val githubService: GithubServ
             .setPrefetchDistance(5)
             .setEnablePlaceholders(true).build()
         return LivePagedListBuilder(factory, config).build()
-    }
-
-    private fun convertData(response: RepoSearchResponse) {
-
     }
 }
